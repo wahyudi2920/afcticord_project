4ce7d58a1f523c8aab2f31677e0a5be6265675ae
Jangki08 <77084787+Jangki08@users.noreply.github.com>
2021-01-07 10:48:33 +0700

    Update helloworld.java
    saya hapus

--- helloworld.java
+++ helloworld.java
@@ -3,14 +3,6 @@ public class HelloWorld{
     System.out.println("Hello World");
     System.out.println("masukan nama :");
     String nama = nextLine();
-    
-    if(nama == wahyudi){
-    System.out.println("tukang gadang");
-    }else if(nama == jangki){
-    System.out.println("gamer(galau merana)");
-    }else{
-    System.out.println("nama tidak terconfirmasi");
-    }
-    
+   
   }
 }
