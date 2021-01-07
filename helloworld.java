9a3b46286d5fe3c11bdc619cfca80bcbef7a1de2
wahyudi2920 <77084980+wahyudi2920@users.noreply.github.com>
2021-01-07 10:47:22 +0700

    Update helloworld.java
    saya menambahkan inputan user dan system if else

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,16 @@
 public class HelloWorld{
   public static void main(){
     System.out.println("Hello World");
+    System.out.println("masukan nama :");
+    String nama = nextLine();
+    
+    if(nama == wahyudi){
+    System.out.println("tukang gadang");
+    }else if(nama == jangki){
+    System.out.println("gamer(galau merana)");
+    }else{
+    System.out.println("nama tidak terconfirmasi");
+    }
+    
   }
 }
